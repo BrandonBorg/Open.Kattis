d6e321b95b021a4bbe4275886251d2be56ab269e
Brandon Borg <brandonjamesborg@gmail.com>
2017-03-30 21:35:42 -0400

    Create 1.2 SolvingForCarrots.java

---
+++ 1.2 SolvingForCarrots.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class SolvingForCarrots {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner (System.in);
+		int n = sc.nextInt();
+		int p = sc.nextInt();
+		for (int i=0; i<n; i++)
+		{
+			String ex = sc.next();
+		}
+		sc.close();
+		System.out.println(p);
+		}
+
+}
