07520fcf3c6e965615a199082ba507847c730449
Brandon Borg <brandonjamesborg@gmail.com>
2018-03-02 17:11:07 -0500

    Update 1.2 nastyhacks.java

--- 1.2 nastyhacks.java
+++ 1.2 nastyhacks.java
@@ -7,7 +7,7 @@ public class nastyhacks {
 		Scanner sc = new Scanner(System.in);
 		int n =sc.nextInt();
 		for (int i=0; i<n;i++)
-			{
+		{
 			int r = sc.nextInt();
 			int e = sc.nextInt();
 			int c = sc.nextInt();
@@ -17,8 +17,9 @@ public class nastyhacks {
 			}else if(r>e-c)
 			{
 				System.out.println("do not advertise");
-			}else {System.out.println("does not matter");
-		}
+			}else {
+				System.out.println("does not matter");
+			}
 		}
 	}
 }
